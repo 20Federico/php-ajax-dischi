abbfe5a18d533a70ce9469c85c6258c1f2c44088
20Federico <fefe.molino@gmail.com>
2021-12-07 14:23:44 +0100

    discs list

---
+++ data/dischi.php
@@ -0,0 +1,67 @@
+<?php
+
+  $discs = [
+    [
+      'imgSrc' => 'bon-jovi-new-jersey.jpg',
+      'title' => 'new jersey',
+      'author' => 'Bon Jovi',
+      'year' => '1988',
+    ],
+    [
+      'imgSrc' => 'queen.jpg',
+      'title' => 'live at wembley 86',
+      'author' => 'Queen',
+      'year' => '1992',
+    ],
+    [
+      'imgSrc' => 'sting.jpg',
+      'title' => 'ten\'s summoner\'s tales',
+      'author' => 'Sting',
+      'year' => '1993',
+    ],
+    [
+      'imgSrc' => 'SteveGadd-800x723.jpg',
+      'title' => 'steve gadd band',
+      'author' => 'Steve Gadd Band',
+      'year' => '2018',
+    ],
+    [
+      'imgSrc' => 'ironmaiden.jpg',
+      'title' => 'brave new world',
+      'author' => 'Iron Maiden',
+      'year' => '2000',
+    ],
+    [
+      'imgSrc' => 'Eric_Clapton_OMCOMR.jpg',
+      'title' => 'one more car, one more raider',
+      'author' => 'Eric Clapton',
+      'year' => '2002',
+    ],
+    [
+      'imgSrc' => 'deeppurple.jpg',
+      'title' => 'made in japan',
+      'author' => 'Deep Purple',
+      'year' => '1972',
+    ],
+    [
+      'imgSrc' => 'metallica.jpg',
+      'title' => 'and justice for all',
+      'author' => 'Metallica',
+      'year' => '1988',
+    ],
+    [
+      'imgSrc' => 'daveweckl.jpg',
+      'title' => 'hard wired',
+      'author' => 'Deve Weckl',
+      'year' => '1994',
+    ],
+    [
+      'imgSrc' => 'michaeljackson.jpg',
+      'title' => 'bad',
+      'author' => 'Michael Jackson',
+      'year' => '1987',
+    ],
+  ];
+
+
+?>
